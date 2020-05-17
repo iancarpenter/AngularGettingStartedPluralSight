391bfd0c5c5061921b26ca03b8d2db7be5ff9f5e
iancarpenter <ianhcarpenter@gmail.com>
2020-05-17 21:47:50 +0100

    Completed version

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './shared/app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,    
+    WelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,    
+    HttpClientModule,    
+    ProductModule,
+    AppRoutingModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
